e3d57b3843cc1b122c2aef04b5cf7cb543b82bb1
Jimmy Baranello <jimmy.baranello@gmail.com>
2021-12-10 12:32:29 -0500

    ui updates

--- wevent/src/components/Event/EventFeed.jsx
+++ wevent/src/components/Event/EventFeed.jsx
@@ -10,24 +10,22 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
-import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import {eventActions} from '../../actions';
 import { connect } from 'react-redux';
 
 
-
 function EventFeed({eventState, event, selectEvent}) {
   var handleSelect = function (e){
     selectEvent(event.event_id);
-    console.log(event, event.selected);
+    console.log(event, event.selected);    
   };
  
   return (
-    <Container maxWidth="50vw">
-    <Card sx={{ maxWidth: '70vw' }}>
-    <h1>{event.event_name}</h1>
+    <Grid fixed maxWidth="sm">
+    <Card sx={{ maxWidth: '70vw', m: 3 }}>
       <CardHeader
         
         action={
@@ -35,8 +33,7 @@ function EventFeed({eventState, event, selectEvent}) {
             {event.selected? <CheckBoxIcon /> : <CheckBoxOutlineBlankIcon/>}
           </IconButton>
         }
-        title={event.name}
-        subheader={event.start}
+        title={event.event_name}
       />
       <CardMedia
         component="img"
@@ -45,22 +42,29 @@ function EventFeed({eventState, event, selectEvent}) {
         alt={event.name}
       />
       <CardContent>
+        <Typography variant="subtitle" color="text.primary">
+          Description:
+          </Typography>
         <Typography variant="body2" color="text.secondary">
-          <h5>Description:</h5> {event.description}
+          {event.description}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-        <h5>Start/End:</h5>
-          <AccessTimeIcon/> {event.start} - {event.end}
+        <Typography variant="subtitle" color="text.primary">
+          Start:
+          </Typography>
+        <Typography variant="body2" color="text.secondary">        
+          <AccessTimeIcon/> {event.start}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-        <h5>Location:</h5>
+        <Typography variant="subtitle" color="text.primary">
+          Location:
+          </Typography>
+        <Typography variant="body2" color="text.secondary">        
           <LocationOnIcon/> {event.full_address}
         </Typography>
         
       </CardContent>
       
     </Card>
-    </Container>
+    </Grid>
   );
 }
 
